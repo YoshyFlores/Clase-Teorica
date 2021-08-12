9771de5123b61af0cd16b154ad44f5fd00b52a84
Yosselin Flores <yoshy.floresg17@gmail.com>
2021-08-12 07:50:03 -1100

    Ejemplos de clases creados

--- Program.cs
+++ Program.cs
@@ -10,6 +10,53 @@ namespace Clase_Teorica
     {
         static void Main(string[] args)
         {
+            //Llamando al metodo loops
+            loops();
+            Console.ReadLine();
         }
+        //Bucles
+        //Voy a crear un metodo que no retornara, tipo void
+        public static void loops()
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 5;
+            numbers[1] = 10;
+            numbers[2] = 15;
+
+            //Ciclo while
+            //mientras sea verdadera se va a ejecutar
+
+            int i = 0;
+            Console.WriteLine("Imprime elementos con While");
+            while (i < numbers.Length)
+            {
+                Console.WriteLine(numbers[i]);
+                i++; //i=i+1
+                //cuantos elementos tiene el arreglo, length
+
+            }
+            //imprime la primera vez 
+            //Se puede usar cuando quiero mostrar una salida en especifico.
+            Console.WriteLine("Imprime elementos con DO- WHILE");
+            int inc = 0;
+            do
+            {
+                Console.WriteLine(numbers[inc]);
+                inc++;
+            } while (10 < numbers.Length);
+
+            Console.WriteLine("IMPRIME CON FOR");
+                for (int j = 0; j < numbers.Length; j++) 
+            {
+                Console.WriteLine("Posicion {0} : {1}", j, numbers[j]);
+            }
+
+            Console.WriteLine("Imprime elementos con FOREACH");
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
     }
 }
